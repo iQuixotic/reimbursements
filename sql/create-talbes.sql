fe309d39357b02e625301d7392a543328456ca47
Trey <treyw1850@gmail.com>
2019-07-07 10:52:51 -0500

    set up seeders

--- sql/create-talbes.sql
+++ sql/create-talbes.sql
@@ -2,50 +2,49 @@ CREATE DATABASE `credits`;
 
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
--- for creating the user table
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+-- for creating the users table
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
   `password` varchar(255) DEFAULT NULL,
   `firstName` varchar(255) DEFAULT NULL,
   `lastName` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
 --   `role` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`userId`)
 );
 
--- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
--- for creating the role table only
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
+-- for creating the roles table only
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
   `roleId` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`roleId`)
 );
 
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  -- for creating the reimbursement table
-DROP TABLE IF EXISTS `reimbursement`;
-CREATE TABLE `reimbursement` (
+  -- for creating the reimbursements table
+DROP TABLE IF EXISTS `reimbursements`;
+CREATE TABLE `reimbursements` (
   `reimbursementId` int(11) NOT NULL AUTO_INCREMENT,
   `author` varchar(255) DEFAULT NULL,
   `amount` varchar(255) DEFAULT NULL,
   `dateSubmitted` varchar(255) DEFAULT NULL,
-  `dateResolved` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `resolver` varchar(255) DEFAULT NULL,
---   `status` int(255) DEFAULT NULL,
---   `type` int(10) DEFAULT NULL,
+  `dateResolved` int(255) DEFAULT NULL,
+  `description` int(255) DEFAULT NULL,
+  `resolver` int(255) DEFAULT NULL,
+  `status` int(255) DEFAULT NULL,
+  `type` int(10) DEFAULT NULL,
   PRIMARY KEY (`reimbursementId`)
 );
 
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-  -- for creating the reimbursementStatus table
-DROP TABLE IF EXISTS `reimbursementStatus`;
-CREATE TABLE `reimbursementStatus` (
+  -- for creating the reimbursementStatuses table
+DROP TABLE IF EXISTS `reimbursementStatuses`;
+CREATE TABLE `reimbursementStatuses` (
   `statusId` int(10) NOT NULL AUTO_INCREMENT,
   `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`statusId`)
@@ -53,9 +52,9 @@ CREATE TABLE `reimbursementStatus` (
 
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-  -- for creating the reimbursementType table
-DROP TABLE IF EXISTS `reimbursementType`;
-CREATE TABLE `reimbursementType` (
+  -- for creating the reimbursementTypes table
+DROP TABLE IF EXISTS `reimbursementTypes`;
+CREATE TABLE `reimbursementTypes` (
   `typeId` int(10) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`typeId`)
--- sql/seeders.sql
+++ sql/seeders.sql
@@ -1,4 +1,50 @@
-
+/*
+    NOTE: I'm not specifying the column names as such: 
+    ---------------------------------------------------------------
+    ----INSERT INTO table_name (column1, column2, column3, ...)----
+    ----VALUES (value1, value2, value3, ...);----------------------
+    ---------------------------------------------------------------
+        
+    REASON: because I will be populating ALL fields in each table,
+        thus making the more verbose syntax unnecessary.
+*/
 
 -- for seeding the user table
-INSERT INTO `reimbursement` VALUES ('1', 'Ip Mann', 'ip@yahoo.com', 'President', '567.565.0697');
+INSERT INTO `users` VALUES (1, 'tomtom', 'Larry', 'Cripsmith', 'fromto@yahoo.com', 4);
+INSERT INTO `users` VALUES (2, 'timtim', 'Crommery', 'Gobbard', 'electionguy_434@yahoo.com', 2);
+INSERT INTO `users` VALUES (3, 'tisktisk', 'Terija', 'Tsu', 'secretsquirrel96@yahoo.com', 5);
+INSERT INTO `users` VALUES (4, 'patpat', 'Patrick', 'Byrde', 'patkumuji@yahoo.com', 3);
+INSERT INTO `users` VALUES (5, 'sarasara', 'Shirley', 'Johnson', 'shirleyj@yahoo.com', 1);
+
+-- for seeding the role table
+INSERT INTO `roles` VALUES (1, 'Manager');
+INSERT INTO `roles` VALUES (2, 'Scrum Master');
+INSERT INTO `roles` VALUES (3, 'Cloud Architect');
+INSERT INTO `roles` VALUES (4, 'Sr. Developer');
+INSERT INTO `roles` VALUES (5, 'Jr. Developer');
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+-- ** I must add a status and type as the last addition in the reimbursement table...
+
+-- for seeding the reimbursements table...
+INSERT INTO `reimbursements` VALUES (1, 'Larry Cripsmith', 500, 06292019, 06302019, 'AWS Sol-Arch Ass. Cert.', 5, 3, 3);
+INSERT INTO `reimbursements` VALUES (2, 'Terija Tsu', 300, 06192019, 06302019, 'AWS Security Cert.', 3, 3, 3);
+INSERT INTO `reimbursements` VALUES (3, 'Terija Tsu', 250, 05262019, 06302019, 'AWS Sol-Arch Pro. Cert.', 4, 1, 2);
+INSERT INTO `reimbursements` VALUES (4, 'Larry', 399, 05292019, 06142019, 'AWS Dev-Ops Cert.', 5, 2, 1);
+INSERT INTO `reimbursements` VALUES (5, 'Larry', 350, 05292019, 06142019, 'AWS Adv. Networking Cert.', 3, 2, 2);
+INSERT INTO `reimbursements` VALUES (6, 'Terija Tsu', 399, 04202019, 04302019, 'AWS Sol-Arch Pro. Cert.', 4, 2, 2);
+INSERT INTO `reimbursements` VALUES (7, 'Larry', 150, 03292019, 04052019, 'Travel', 4, 1, 2);
+INSERT INTO `reimbursements` VALUES (8, 'Larry', 420, 02292019, 04052019, 'AWS Adv. Networking Cert.', 2, 1, 1);
+
+-- for seeding the reimbursementStatuses table
+INSERT INTO `reimbursementStatuses` VALUES (1, 'fufilled');
+INSERT INTO `reimbursementStatuses` VALUES (2, 'in-transit');
+INSERT INTO `reimbursementStatuses` VALUES (3, 'pending');
+
+-- for seeding the reimbursementTypes table...
+INSERT INTO `reimbursementTypes` VALUES (1, 'cash');
+INSERT INTO `reimbursementTypes` VALUES (2, 'credit');
+INSERT INTO `reimbursementTypes` VALUES (3, 'unfufilled');
+
