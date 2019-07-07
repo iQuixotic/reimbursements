014098d2aea09442ea3163db48e1b51260a4aee2
Trey <treyw1850@gmail.com>
2019-07-07 08:48:48 -0500

    set up db connection config

---
+++ config/connection.ts
@@ -0,0 +1,12 @@
+var {Client} = require('pg');
+var SECRET = require('./secret');
+
+const client = new Client({
+    user: SECRET.DB_USER,
+    password:SECRET.DB_PASSWORD,
+    host: SECRET.DB_HOST,
+    port: SECRET.DB_PORT,
+    database: SECRET.DB_DATABASE
+});
+
+module.exports = client;
