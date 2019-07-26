31f3251655021683c8684a1e48a045ff1c2b3bea
Trey <treyw1850@gmail.com>
2019-07-26 13:13:29 -0500

    added register page input fields

--- client/src/containers/pgs/register.tsx
+++ client/src/containers/pgs/register.tsx
@@ -12,16 +12,42 @@ class RegisterPg extends React.Component{
   // -----------------------------------
   public render() {
       return(
-          <Layout {...this.props} {...this.state}>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-        {this.state.message}
+        <Layout {...this.props} {...this.state}>
+        Hello, I am the login !!!
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+            {this.state.message}
 
-          </Layout>
+            <div className="login-inputs-div">
+                <label htmlFor="username">Email: </label>
+                <input name='username' type="text"/>
+
+                <label htmlFor="password">Password: </label>
+                <input type='password' name='password'/>
+
+                <label htmlFor="passwordConfirm">Confirm Password: </label>
+                <input type='password' name='passwordConfirm'/>
+
+                <label htmlFor="firstName">First Name: </label>
+                <input type='password' name='firstName'/>
+
+                <label htmlFor="lastName">Last Name: </label>
+                <input type='password' name='lastName'/>
+
+                {/* make a role 1-3 selection box here */}
+
+                <a href="#">Forgot Password...</a>
+                <div className="login-and-register-btns">
+                    <button>Login</button>
+                    <button>Register</button>
+                </div>
+            </div>
+
+    </Layout>
       )
   }
 }
